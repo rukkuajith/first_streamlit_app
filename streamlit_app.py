85f8dd50fdc8b27259a32da48920f7f505e71d3a
Rukku Ajith <105693392+rukkuajith@users.noreply.github.com>
2023-02-16 19:21:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('BreakFast Menu')
+streamlit.text('Omega 3 & Blueberry Otameal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled & Free-Range Egg')
